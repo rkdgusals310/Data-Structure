3dfd30883ccfca92702c17417113fc4f8de26c53
LMS1229 <asker5325@naver.com>
2022-07-05 12:22:48 +0900

    1주차 과제 및 과제 샘플

---
+++ 과제/1주차/Sample/array_sample.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct __int_array{
+
+}int_array;
+
+void init(int_array* array, const int capacity);    //스택 초기화
+int peek(int_array* array, const int index);
+int insert(int_array* array, const int index, const int value);
+void push(int_array* array, const int value);
+int delete(int_array* array, const int index);
