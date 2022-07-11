88cc30dd2e71c4c4dfdd9e6b300cc1aee4041757
hmooko <103720259+hmooko@users.noreply.github.com>
2022-07-11 17:19:31 +0900

    1주차 과제 업로드

---
+++ 현모/1주차/array_sample.c
@@ -0,0 +1,72 @@
+#include "array_sample.h"
+#include <stdlib.h>
+
+void init(int_array* array, const int capacity)
+{
+    array->capacity = capacity;
+    array->size = 0;
+    array->arr = (int*)malloc(sizeof(int) * capacity);
+}
+
+int peek(int_array* array, const int index)
+{
+    if (array->size >= index && index < 0)
+    {
+        exit(0);
+    }
+    else
+    {
+        return array->arr[index];
+    }
+}
+
+int insert(int_array *array, const int index, const int value)
+{
+    if (array->size < index && index < 0)
+    {
+        return 0;
+    }
+
+    if (array->size == array->capacity)
+    {
+        array->arr = (int*)realloc(array->arr, sizeof(int) * array->capacity * 2);
+        array->capacity = array->capacity * 2;
+    }
+
+    for (int i = array->size; i > index; i--)
+    {
+        array->arr[i] = array->arr[i - 1];
+    }
+    array->arr[index] = value;
+    array->size++;
+
+    return 1;
+} 
+
+void push(int_array* array, const int value)
+{
+    if (array->size == array->capacity)
+    {
+        array->arr = (int*)realloc(array->arr, sizeof(int) * array->capacity * 2);
+        array->capacity = array->capacity * 2;
+    }
+
+    array->arr[array->size] = value;
+    array->size++;
+}
+
+int delete (int_array *array, const int index)
+{
+    if (array->arr[0] != NULL || (array->size < index && index < 0))
+    {
+        return 0; 
+    }
+
+    for (int i = index; i < array->size - 1; i++)
+    {
+        array->arr[i] = array->arr[i + 1];
+    }
+    array->size--;
+
+    return 1;
+} 
