6fbf097c5afe6aa2a5c535619312599215b42f1d
hmooko <103720259+hmooko@users.noreply.github.com>
2022-07-12 16:48:22 +0900

    1주차 과제 제출

--- 현모/1주차/array_sample.h
+++ 현모/1주차/array_sample.h
@@ -7,8 +7,9 @@ typedef struct __int_array
     int capacity;
 } int_array;
 
-void init(int_array* array, const int capacity); //�迭 �ʱ�ȭ
+void init(int_array* array, const int capacity); //¹è¿­ ÃÊ±âÈ­
 int peek(int_array* array, const int index);
 int insert(int_array* array, const int index, const int value);
 void push(int_array* array, const int value);
 int delete (int_array* array, const int index);
+
